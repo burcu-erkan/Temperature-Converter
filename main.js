e6111c5e7746f434098b841aa84cde90d28dedb0
burcu-erkan <burcuerkan.86@gmail.com>
2021-03-02 09:54:16 -0800

    done

---
+++ main.js
@@ -0,0 +1,9 @@
+var celcius= 32;
+
+var fahrenheit = (celcius*9)/5 + 32;
+console.log(celcius+'\u00B0C' + ' is '+fahrenheit+'\u00B0F');
+
+var fahrenheit = 380;
+var celcius = (fahrenheit-32)*5 /9;
+
+console.log(fahrenheit+'\u00B0F ' + 'is '+celcius+'\u00B0C');
